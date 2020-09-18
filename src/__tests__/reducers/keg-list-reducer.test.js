3b6f17d11255131f68674b8ca456d22d2f8cb4a9
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-18 14:11:51 -0700

    continue troubleshooting failed test to make it pass

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,16 +1,25 @@
+import { Alert } from 'react-bootstrap';
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
 
-  let action;
-  const sampleKegData = {
+  const currentState = {
+    1: {
     name: "Blue Milk",
     brand: "Twin Suns Inc.",
     origin: "Tatooine",
-    pintsRemaining: "124",
+    pintsRemaining: 124,
     price: "4.00",
-    id: 1
-  };
+    id: 1 },
+    2: {
+    name: "Gamorrean Ale",
+    brand: "Warlords Choice",
+    origin: "Gamorr",
+    pintsRemaining: 124,
+    price: "3.00",
+    id: 2
+    }
+  }
 
   test('Should return default state if there is no action type passed into the reducer',() => {
     expect(kegListReducer({}, { type: null })).toEqual({});
@@ -37,6 +46,16 @@ describe('kegListReducer', () => {
         id: id
       }
     });
+
+    test('Should successfully delete a ticket', () => {
+      action = {
+        type: 'DELETE_KEG',
+        id: 1
+      };
+      expect(kegListReducer(currentState, action)).toEqual({
+        2: {name: ''}
+      })
+    })
   });
-  
+
 });
