6ffb6ebcb106fe06e97fb7a824f692f5c14c3f47
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-18 16:37:17 -0700

    begin adding redux by adding store and create store and provider to the index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/keg-list-reducer';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
