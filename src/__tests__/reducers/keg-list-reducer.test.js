476a0b25234da1f430ce8be141154c75abe9ff9f
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-18 13:49:36 -0700

    show a good fail for my second test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,42 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const sampleKegData = {
+    name: "Blue Milk",
+    brand: "Twin Suns Inc.",
+    origin: "Tatooine",
+    pintsRemaining: "124",
+    price: "4.00",
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer',() => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, origin, pintsRemaining, price, id } = sampleKegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      origin: origin,
+      pintsRemaining: pintsRemaining,
+      price: price,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        origin: origin,
+        pintsRemaining: pintsRemaining,
+        price: price,
+        id: id
+      }
+    });
+  });
+  
 });
