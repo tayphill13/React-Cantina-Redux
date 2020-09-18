3469e5cd450d03e6fac27936c6fb6d8fcb182a2f
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-18 13:39:10 -0700

    fail test successfully, not to add code to pass

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,1 +1,7 @@
-import kegListReducer from '/../../reducers/keg-list-reducer'
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+describe('kegListReducer', () => {
+  test('Should return default state if there is no action type passed into the reducer',() => {
+    expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+});
