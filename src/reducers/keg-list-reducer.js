40de97bf60937209bbcb23a58386511120839442
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-18 13:53:51 -0700

    confirm that my second test will pass

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-    return state;
+  const { name, brand, origin, pintsRemaining, price, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          origin: origin,
+          pintsRemaining: pintsRemaining,
+          price: price,
+          id: id
+        }
+      });
+    default:
+      return state;
+    }
 };
